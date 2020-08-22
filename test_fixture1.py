8291cba7bd5f7f53a874ba9711079808f01dc425
admin <vovnik98@mail.ru>
2020-08-23 02:43:31 +0300

    Добавлены новые файлы
    Signed-off-by: admin <vovnik98@mail.ru>

---
+++ test_fixture1.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+class TestMainPage1():
+
+    @classmethod
+    def setup_class(self):
+        print("\nСейчас я тут 1")
+        self.browser = webdriver.Chrome("chromedriver.exe")
+
+    @classmethod
+    def teardown_class(self):
+        print("Сейчас я тут 2")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        print('Сейчас я тут 3')
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('Сейчас я тут 4')
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+
+class TestMainPage2():
+
+    def setup_method(self):
+        print("start 1")
+        self.browser = webdriver.Chrome()
+
+    def teardown_method(self):
+        print("quit 2")
+        self.browser.quit()
+
+    def test_guest_should_see_login_link(self):
+        print('start 3')
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('start 4')
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector(".basket-mini .btn-group > a")
